746c5240ef684149a7434c2f12e5a2403ac10def
nasrulain <nasrulain.md@gmail.com>
2020-03-28 18:56:28 +0800

    3 new files added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Peer-graded assignment - guessinggame.sh for The Unix Workbench course
+
+## By Coursera/Johns Hopkins University
+
+README.md created: Sat Mar 28 18:54:40 DST 2020
+
+Number of lines in guessinggame.sh: 29
---
+++ guessinggame.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+# Peer graded assignment - The Unix Workbench by Coursera/John's Hopkins University
+
+# Counting  no. of files in the directory
+filecount=$(ls -1 | wc -l)
+
+# Function with IF condition to check guessed value
+function output {
+	    	  if [[ $guesscount -lt $filecount ]]
+	          then
+		    echo "Too low!"
+	          elif [[ $guesscount -gt $filecount ]]
+	          then
+		    echo "Too high!"
+	          else
+		    echo ""
+		    echo "Well done! You guess is correct!"
+	          fi
+	        }
+
+# While loop
+while [[ $filecount -ne $guesscount ]]
+  do
+	read -p "Guess how many files are in the current directory? " guesscount
+	output
+	echo ""
+	done
+
